a13b80d301d616fdfb63a59fda8d58c6a41887f2
tabatamelisa <tabatavernaza12@gmail.com>
2025-03-13 13:14:35 -0500

    primer commit

---
+++ ParesEImpares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParesEImpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Validar si un número es oar o impar
+            //Andy Garzón
+            int numero;
+            do
+            {
+                Console.Write("Digita un número entero: "); //el usuario debe ingresar el número
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("El número ingresado es par.");
+                }
+                else
+                {
+                    Console.WriteLine("El número ingresado es impar.");
+                }
+            } while (true); //Se repite el prgrama
+        }
+    }
+}
